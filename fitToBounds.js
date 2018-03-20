28f1919b4458a89e4810868183cdfc3baeb85162
Mozart Diniz <mozart.diniz@gmail.com>
2018-03-20 08:20:47 +0100

    jump menu

--- fitToBounds.js
+++ fitToBounds.js
@@ -65,6 +65,7 @@ const fitToBounds = (map, e) => {
             right: 80,
             bottom: 80,
             left: 80
-        }
+        },
+        speed: 2
     });
 }
--- showClusterLabel.js
+++ showClusterLabel.js
@@ -9,9 +9,22 @@ const showClusterLabel = (e, map, popup, cluster) => {
 
     const clusterId = features[0].properties.cluster_id;
     const allFeatures = cluster.getLeaves(clusterId, Math.floor(map.getZoom()), Infinity);
-    const featuresNames = allFeatures.map((feature) => feature.properties.name);
+    const fragment = document.createDocumentFragment();
+    const featuresNames = allFeatures.forEach((feature) => {
+      const listItem = document.createElement('div');
+      listItem.addEventListener('click', () => {
+        map.flyTo({
+          center: feature.geometry.coordinates,
+          zoom: 16,
+          speed: 2
+        });
+      });
+
+      listItem.innerHTML = feature.properties.name;
+      fragment.appendChild(listItem);
+    });
 
     popup.setLngLat(features[0].geometry.coordinates)
-        .setHTML(featuresNames.join('<br >'))
+        .setDOMContent(fragment)
         .addTo(map);
 }
