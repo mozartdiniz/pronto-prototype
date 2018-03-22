71d8bdf4502ad2832928b392ca07b785ac076a67
Mozart Diniz <mozart.diniz@gmail.com>
2018-03-22 10:00:43 +0100

    full screen

--- kiosk/index.js
+++ kiosk/index.js
@@ -4,12 +4,12 @@ let maxSize = 0;
 let alerts = [];
 
 const updateAlertDetails = (alert) => {
-    const shipName = document.querySelector('.value.ship-name');
-    const berthName = document.querySelector('.value.berth');
-    const alertTitle = document.querySelector('.alert-title');
     const alertDescription = document.querySelector('.alert-text');
-    const alertDate = document.querySelector('.notifications .dates .date .label');
-    const alertTime = document.querySelector('.notifications .dates .hour .label');    
+    const shipName         = document.querySelector('.value.ship-name');
+    const berthName        = document.querySelector('.value.berth');
+    const alertTitle       = document.querySelector('.alert-title');    
+    const alertDate        = document.querySelector('.notifications .dates .date .label');
+    const alertTime        = document.querySelector('.notifications .dates .hour .label');    
 
     shipName.innerHTML = alert.properties.name;
     berthName.innerHTML = alert.properties.berth;
@@ -89,28 +89,53 @@ const showInfoArea = (show) => {
     el.style.display = (show) ? 'block' : 'none';
 }
 
+function toggleFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+
+    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+        requestFullScreen.call(docEl);
+    }
+    else {
+        cancelFullScreen.call(doc);
+    }
+}
+
+const addFullscreenEvent = () => {
+    const el = document.querySelector('.fullscreen-mode');
+    el.addEventListener('click', toggleFullScreen);
+}
+
+const init = () => {
+    map.fitBounds(getCoordinates(alerts), {
+        padding : {
+            top: 80,
+            right: 80,
+            bottom: 80,
+            left: 80
+        },
+        speed: 2
+    });        
+
+    populateMap(alerts);
+    updateScreenDate();
+    updateScreenTime();
+    updateAlertCounter();
+    addFullscreenEvent();
+
+    setInterval(updateScreenDate, 3600000);
+    setInterval(updateScreenTime, 1000);
+    setInterval(consumeAlerts, 10000);
+}
+
 fetch('alerts.json')
     .then((response) => response.json())
     .then((data) => {
         alerts = data;
         maxSize = data.length;
 
-        map.fitBounds(getCoordinates(alerts), {
-            padding : {
-                top: 80,
-                right: 80,
-                bottom: 80,
-                left: 80
-            },
-            speed: 2
-        });        
-
-        populateMap(alerts);
-        updateScreenDate();
-        updateScreenTime();
-        updateAlertCounter();
-
-        setInterval(updateScreenDate, 3600000);
-        setInterval(updateScreenTime, 1000);
-        setInterval(consumeAlerts, 10000);
+        init();
     });
